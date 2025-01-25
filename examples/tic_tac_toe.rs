b01ce22a4535315578cd9f8b471879db3e6e2803
Payton Webber <53197664+PaytonWebber@users.noreply.github.com>
2025-01-25 00:55:05 -0800

    refactor tic-tac-toe example to use bitboard representation (#4)

--- examples/tic_tac_toe.rs
+++ examples/tic_tac_toe.rs
@@ -1,42 +1,43 @@
-use rand::seq::SliceRandom;
-
 use mcts_rs::{Mcts, State};
 
 #[derive(Debug, Clone)]
 pub struct TicTacToe {
-    board: [[[u8; 2]; 3]; 3],
-    pub current_player: usize,
-    pub legal_actions: Vec<(usize, usize)>,
+    board_x: u16,
+    board_o: u16,
+    // Current player: 0 => X, 1 => O
+    current_player: usize,
+    // Row, col
+    legal_actions: Vec<(u8, u8)>,
 }
 
-const WINNER_MASK: [[(usize, usize); 3]; 8] = [
-    // Rows
-    [(0, 0), (0, 1), (0, 2)],
-    [(1, 0), (1, 1), (1, 2)],
-    [(2, 0), (2, 1), (2, 2)],
-    // Columns
-    [(0, 0), (1, 0), (2, 0)],
-    [(0, 1), (1, 1), (2, 1)],
-    [(0, 2), (1, 2), (2, 2)],
-    // Diagonals
-    [(0, 0), (1, 1), (2, 2)],
-    [(0, 2), (1, 1), (2, 0)],
+const WIN_PATTERNS: [u16; 8] = [
+    // rows
+    0b111000000,
+    0b000111000,
+    0b000000111,
+    // cols
+    0b100100100,
+    0b010010010,
+    0b001001001,
+    // diags
+    0b100010001,
+    0b001010100,
 ];
 
 impl State for TicTacToe {
-    type Action = (usize, usize);
+    type Action = (u8, u8);
 
     fn default_action() -> Self::Action {
         (0, 0)
     }
 
     fn player_has_won(&self, player: usize) -> bool {
-        for line in WINNER_MASK.iter() {
-            let [(i0, j0), (i1, j1), (i2, j2)] = *line;
-            if self.board[i0][j0][player] == 1
-                && self.board[i1][j1][player] == 1
-                && self.board[i2][j2][player] == 1
-            {
+        let board = match player {
+            0_usize => self.board_x,
+            _ => self.board_o,
+        };
+        for &pattern in WIN_PATTERNS.iter() {
+            if (board & pattern) == pattern {
                 return true;
             }
         }
@@ -44,9 +45,12 @@ impl State for TicTacToe {
     }
 
     fn is_terminal(&self) -> bool {
-        self.player_has_won(0) || self.player_has_won(1) || self.legal_actions.is_empty()
+        self.player_has_won(0)
+            || self.player_has_won(1)
+            || board_is_filled(self.board_x, self.board_o)
     }
-    fn get_legal_actions(&self) -> Vec<(usize, usize)> {
+
+    fn get_legal_actions(&self) -> Vec<Self::Action> {
         self.legal_actions.clone()
     }
 
@@ -54,27 +58,25 @@ impl State for TicTacToe {
         self.current_player
     }
 
-    fn step(&self, action: (usize, usize)) -> Self {
-        let mut new_board = self.board;
-        new_board[action.0][action.1][self.current_player] = 1;
-
-        // Create a new vector excluding the taken action
-        let mut new_legal_actions = Vec::with_capacity(self.legal_actions.len() - 1);
-        for &a in &self.legal_actions {
-            if a != action {
-                new_legal_actions.push(a);
-            }
+    fn step(&self, action: Self::Action) -> Self {
+        let mut new_board_x = self.board_x;
+        let mut new_board_o = self.board_o;
+        if self.current_player == 0 {
+            set_bit(&mut new_board_x, action.0 * 3 + action.1);
+        } else {
+            set_bit(&mut new_board_o, action.0 * 3 + action.1);
         }
-
+        let current_player = 1 - self.current_player;
+        let legal_actions = determine_legal_actions(new_board_x, new_board_o);
         TicTacToe {
-            board: new_board,
-            current_player: 1 - self.current_player,
-            legal_actions: new_legal_actions,
+            board_x: new_board_x,
+            board_o: new_board_o,
+            current_player,
+            legal_actions,
         }
     }
 
     fn reward(&self, to_play: usize) -> f32 {
-        assert!(self.is_terminal());
         if self.player_has_won(to_play) {
             -1.0
         } else if self.player_has_won(1 - to_play) {
@@ -86,65 +88,74 @@ impl State for TicTacToe {
 
     fn render(&self) {
         println!("X: player 0, O: player 1\n");
-        for i in 0..3 {
-            let mut current_line: Vec<String> = Vec::new();
+        for i in (0..3).rev() {
+            let mut current_line: Vec<String> = Vec::with_capacity(3);
             for j in 0..3 {
-                if self.board[i][j][0] == 1 {
+                let pos = i * 3 + j;
+
+                let mask = 1 << pos;
+                if (self.board_x & mask) != 0 {
                     current_line.push("X".to_string());
-                } else if self.board[i][j][1] == 1 {
+                } else if (self.board_o & mask) != 0 {
                     current_line.push("O".to_string());
                 } else {
                     current_line.push(" ".to_string());
                 }
             }
             println!(
-                "{} | {} | {}",
+                " {} | {} | {}",
                 current_line[0], current_line[1], current_line[2]
             );
-            if i != 2 {
+            if i > 0 {
                 println!("---------");
-            } else {
-                println!("\n");
             }
         }
+        println!();
     }
 }
 
 impl TicTacToe {
-    fn determine_legal_actions(board: &[[[u8; 2]; 3]; 3]) -> Vec<(usize, usize)> {
-        let mut legal_actions = Vec::new();
+    fn new() -> TicTacToe {
+        let mut legal_actions: Vec<(u8, u8)> = Vec::with_capacity(9);
         for i in 0..3 {
             for j in 0..3 {
-                if board[i][j][0] == 0 && board[i][j][1] == 0 {
-                    legal_actions.push((i, j));
-                }
+                legal_actions.push((i, j));
             }
         }
-        legal_actions
+        TicTacToe {
+            board_x: 0,
+            board_o: 0,
+            current_player: 0,
+            legal_actions,
+        }
     }
 }
 
-impl Default for TicTacToe {
-    fn default() -> Self {
-        let board = [[[0; 2]; 3]; 3];
-        TicTacToe {
-            board,
-            current_player: 0,
-            legal_actions: TicTacToe::determine_legal_actions(&board),
+fn determine_legal_actions(board_x: u16, board_o: u16) -> Vec<(u8, u8)> {
+    let mut actions: Vec<(u8, u8)> = Vec::with_capacity(9);
+    for pos in 0..9 {
+        let mask = 1 << pos;
+        if (board_x & mask) == 0 && (board_o & mask) == 0 {
+            actions.push((pos / 3, pos % 3));
         }
     }
+    actions
+}
+
+fn board_is_filled(board_x: u16, board_o: u16) -> bool {
+    (board_x | board_o) == 0x1FF
+}
+
+fn set_bit(board: &mut u16, pos: u8) {
+    *board |= 1 << pos;
 }
 
 fn main() {
-    let mut game = TicTacToe::default();
-
-    // Randomly select the first action
-    let action = game.legal_actions.choose(&mut rand::thread_rng()).unwrap();
-    game = game.step(*action);
+    let mut game = TicTacToe::new();
 
     while !game.is_terminal() {
-        let mut mcts = Mcts::new(game.clone(), 5.0);
-        let action = mcts.search(100000);
+        let mut mcts = Mcts::new(game.clone(), 0.5);
+        let action = mcts.search(10000);
         game = game.step(action);
         game.render();
     }
