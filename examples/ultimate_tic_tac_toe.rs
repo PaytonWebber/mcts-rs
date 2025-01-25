5298868c04106e2d4093985b4769c7c67e88efd6
Payton Webber <53197664+PaytonWebber@users.noreply.github.com>
2025-01-25 14:30:06 -0800

    refactor ultimate-tic-tac-toe example to be more readable, and fix logic error (#5)

--- examples/ultimate_tic_tac_toe.rs
+++ examples/ultimate_tic_tac_toe.rs
@@ -1,79 +1,81 @@
 use mcts_rs::{Mcts, State};
-use rand::seq::SliceRandom;
 
 #[derive(Debug, Clone)]
 struct UltimateTicTacToe {
-    board: [[[[u8; 2]; 3]; 3]; 9],
-    macro_board: [[[u8; 2]; 3]; 3],
-    player: u8,
+    board: [u8; 81],
+    macro_board: [u8; 9],
+    current_player: usize,
     legal_actions: Vec<(usize, usize, usize)>,
 }
 
-const WINNER_MASK: [[(usize, usize); 3]; 8] = [
-    // Rows
-    [(0, 0), (0, 1), (0, 2)],
-    [(1, 0), (1, 1), (1, 2)],
-    [(2, 0), (2, 1), (2, 2)],
-    // Columns
-    [(0, 0), (1, 0), (2, 0)],
-    [(0, 1), (1, 1), (2, 1)],
-    [(0, 2), (1, 2), (2, 2)],
-    // Diagonals
-    [(0, 0), (1, 1), (2, 2)],
-    [(0, 2), (1, 1), (2, 0)],
-];
+const P1: u8 = 1;
+const P2: u8 = 2;
+const TIE: u8 = 3;
 
 impl State for UltimateTicTacToe {
-    type Action = (usize, usize, usize);
-
+    type Action = (usize, usize, usize); // Mini-board, Row, Col
     fn default_action() -> Self::Action {
         (0, 0, 0)
     }
 
-    fn step(&self, action: (usize, usize, usize)) -> Self {
-        let mut new_board = self.board.clone();
-        new_board[action.0][action.1][action.2][self.player as usize] = 1;
-        let legal_actions =
-            if UltimateTicTacToe::mini_board_full(&new_board, action.1 * 3 + action.2) {
-                UltimateTicTacToe::find_legal_actions(&new_board, -1)
-            } else {
-                UltimateTicTacToe::find_legal_actions(&new_board, (action.1 * 3 + action.2) as i8)
-            };
-        let mut new_macro_board = self.macro_board.clone();
-        let new_player = 1 - self.player;
+    fn player_has_won(&self, player: usize) -> bool {
+        let board = &self.macro_board;
+        let player_val = get_player_val(player);
+        for i in 0..3 {
+            // Row check
+            if board[i * 3] == player_val
+                && board[i * 3 + 1] == player_val
+                && board[i * 3 + 2] == player_val
+            {
+                return true;
+            }
 
-        for board_row in 0..3 {
-            for board_col in 0..3 {
-                let mut local_win = false;
-
-                for line in WINNER_MASK.iter() {
-                    let [(i0, j0), (i1, j1), (i2, j2)] = *line;
-                    if new_board[board_row * 3 + board_col][i0][j0][new_player as usize] == 1
-                        && new_board[board_row * 3 + board_col][i1][j1][new_player as usize] == 1
-                        && new_board[board_row * 3 + board_col][i2][j2][new_player as usize] == 1
-                    {
-                        local_win = true;
-                        break;
-                    }
-                }
-
-                // If a player has won in a local board, mark it in a macro board
-                if local_win {
-                    new_macro_board[board_row][board_col][new_player as usize] = 1;
-                }
+            // Col check
+            if board[i] == player_val && board[i + 3] == player_val && board[i + 6] == player_val {
+                return true;
             }
         }
+        if (board[0] == player_val && board[4] == player_val && board[8] == player_val)
+            || (board[2] == player_val && board[4] == player_val && board[6] == player_val)
+        {
+            return true;
+        }
+        false
+    }
+
+    fn is_terminal(&self) -> bool {
+        self.legal_actions.len() == 0 || self.player_has_won(0) || self.player_has_won(1)
+    }
+
+    fn get_legal_actions(&self) -> Vec<Self::Action> {
+        self.legal_actions.clone()
+    }
+
+    fn to_play(&self) -> usize {
+        self.current_player
+    }
+
+    fn step(&self, action: Self::Action) -> Self {
+        let mut board_clone = self.board.clone();
+        let current_player = 1 - self.current_player;
+        let player_val = get_player_val(current_player);
+        board_clone[action.0 * 9 + action.1 * 3 + action.2] = player_val;
+
+        let mut macro_clone = self.macro_board.clone();
+        update_macro_board(&mut macro_clone, &board_clone, action.0);
+
+        let legal_actions =
+            determine_legal_actions(action.1 * 3 + action.2, &macro_clone, &board_clone);
 
         UltimateTicTacToe {
-            board: new_board,
-            macro_board: new_macro_board,
-            player: new_player,
+            board: board_clone,
+            macro_board: macro_clone,
+            current_player,
             legal_actions,
         }
     }
 
     fn reward(&self, to_play: usize) -> f32 {
-        assert!(self.is_terminal());
         if self.player_has_won(to_play) {
             -1.0
         } else if self.player_has_won(1 - to_play) {
@@ -87,7 +89,6 @@ impl State for UltimateTicTacToe {
         println!("X: player 0, O: player 1\n");
 
         for big_row in 0..3 {
-            // Print each line within the three rows of sub-boards
             for sub_row in 0..3 {
                 let mut current_lines: Vec<String> = Vec::new();
 
@@ -95,15 +96,15 @@ impl State for UltimateTicTacToe {
                     let mut sub_line = String::new();
 
                     for sub_col in 0..3 {
-                        if self.board[big_row * 3 + big_col][sub_row][sub_col][0] == 1 {
-                            sub_line.push('X');
-                        } else if self.board[big_row * 3 + big_col][sub_row][sub_col][1] == 1 {
-                            sub_line.push('O');
-                        } else {
-                            sub_line.push(' ');
+                        // Calculate the index in the 1D array
+                        let index = (big_row * 3 + big_col) * 9 + sub_row * 3 + sub_col;
+
+                        match self.board[index] {
+                            1 => sub_line.push('X'),
+                            2 => sub_line.push('O'),
+                            _ => sub_line.push(' '),
                         }
 
-                        // Add space between cells within a sub-board
                         if sub_col < 2 {
                             sub_line.push('|');
                         }
@@ -112,14 +113,12 @@ impl State for UltimateTicTacToe {
                     current_lines.push(sub_line);
                 }
 
-                // Join the lines of the three sub-boards side by side
                 println!(
                     " {} || {} || {}",
                     current_lines[0], current_lines[1], current_lines[2]
                 );
             }
 
-            // Divider between the large 3x3 sub-boards
             if big_row < 2 {
                 println!("=======||=======||========");
             }
@@ -127,38 +126,84 @@ impl State for UltimateTicTacToe {
 
         println!();
     }
+}
 
-    fn to_play(&self) -> usize {
-        self.player as usize
+fn get_player_val(player: usize) -> u8 {
+    if player == 0 {
+        return P1;
+    } else if player == 1 {
+        return P2;
     }
+    u8::MAX
+}
 
-    fn is_terminal(&self) -> bool {
-        self.player_has_won(0) || self.player_has_won(1) || self.legal_actions.is_empty()
-    }
-
-    // TODO: change this so that I only need to call it once
-    fn player_has_won(&self, player: usize) -> bool {
-        for line in WINNER_MASK.iter() {
-            let [(i0, j0), (i1, j1), (i2, j2)] = *line;
-            if self.macro_board[i0][j0][player] == 1
-                && self.macro_board[i1][j1][player] == 1
-                && self.macro_board[i2][j2][player] == 1
-            {
-                return true;
-            }
+fn update_macro_board(macro_board: &mut [u8; 9], full_board: &[u8; 81], macro_idx: usize) {
+    let offset = macro_idx * 9;
+    let sub_board = &full_board[offset..offset + 9];
+    let mut winner = 0;
+    for i in 0..3 {
+        if sub_board[i * 3] != 0
+            && sub_board[i * 3] == sub_board[i * 3 + 1]
+            && sub_board[i * 3 + 1] == sub_board[i * 3 + 2]
+        {
+            winner = sub_board[i * 3];
+            break;
         }
-        false
+        if sub_board[i] != 0
+            && sub_board[i] == sub_board[i + 3]
+            && sub_board[i + 3] == sub_board[i + 6]
+        {
+            winner = sub_board[i];
+            break;
+        }
     }
-
-    fn get_legal_actions(&self) -> Vec<(usize, usize, usize)> {
-        self.legal_actions.clone()
+    if winner == 0 {
+        if sub_board[0] != 0 && sub_board[0] == sub_board[4] && sub_board[4] == sub_board[8] {
+            winner = sub_board[0];
+        } else if sub_board[2] != 0 && sub_board[2] == sub_board[4] && sub_board[4] == sub_board[6]
+        {
+            winner = sub_board[2];
+        }
+    }
+    if winner == P1 || winner == P2 {
+        macro_board[macro_idx] = winner;
+        return;
+    }
+    if sub_board.iter().all(|&cell| cell != 0) {
+        macro_board[macro_idx] = TIE;
     }
 }
 
-impl UltimateTicTacToe {
-    fn new() -> Self {
-        let mut legal_actions = Vec::with_capacity(81);
+fn determine_legal_actions(
+    next_sub_board: usize,
+    macro_board: &[u8; 9],
+    full_board: &[u8; 81],
+) -> Vec<(usize, usize, usize)> {
+    let mut actions = Vec::new();
+    if macro_board[next_sub_board] == 0 {
+        let start_index = next_sub_board * 9;
+        for i in 0..9 {
+            if full_board[start_index + i] == 0 {
+                actions.push((next_sub_board, i / 3, i % 3));
+            }
+        }
+    } else {
+        // We can play on any empty cell in the entire board
+        for i in 0..81 {
+            if full_board[i] == 0 {
+                let board_idx = i / 9;
+                let row = (i % 9) / 3;
+                let col = (i % 9) % 3;
+                actions.push((board_idx, row, col));
+            }
+        }
+    }
+    actions
+}
 
+impl UltimateTicTacToe {
+    pub fn new() -> UltimateTicTacToe {
+        let mut legal_actions: Vec<(usize, usize, usize)> = Vec::with_capacity(81);
         for i in 0..9 {
             for j in 0..3 {
                 for k in 0..3 {
@@ -167,63 +212,18 @@ impl UltimateTicTacToe {
             }
         }
         UltimateTicTacToe {
-            board: [[[[0; 2]; 3]; 3]; 9],
-            macro_board: [[[0; 2]; 3]; 3],
-            player: 0,
+            board: [0; 81],
+            macro_board: [0; 9],
+            current_player: 0,
             legal_actions,
         }
-    }
-
-    fn find_legal_actions(
-        board: &[[[[u8; 2]; 3]; 3]; 9],
-        mini_idx: i8,
-    ) -> Vec<(usize, usize, usize)> {
-        let mut legal_actions = Vec::new();
-
-        // Check all mini boards
-        if mini_idx == -1 {
-            for i in 0..9 {
-                for j in 0..3 {
-                    for k in 0..3 {
-                        if board[i][j][k][0] == 0 && board[i][j][k][1] == 0 {
-                            legal_actions.push((i, j, k));
-                        }
-                    }
-                }
-            }
-        } else {
-            for i in 0..3 {
-                for j in 0..3 {
-                    if board[mini_idx as usize][i][j][0] == 0
-                        && board[mini_idx as usize][i][j][1] == 0
-                    {
-                        legal_actions.push((mini_idx as usize, i, j));
-                    }
-                }
-            }
-        }
-        legal_actions
-    }
-
-    fn mini_board_full(board: &[[[[u8; 2]; 3]; 3]; 9], board_idx: usize) -> bool {
-        for i in 0..3 {
-            for j in 0..3 {
-                if board[board_idx][i][j][0] == 0 && board[board_idx][i][j][0] == 0 {
-                    return false;
-                }
-            }
-        }
-        true
     }
 }
 
 fn main() {
     let mut game = UltimateTicTacToe::new();
-    let action = game.legal_actions.choose(&mut rand::thread_rng()).unwrap();
-    game = game.step(*action);
-
     while !game.is_terminal() {
-        let mut mcts = Mcts::new(game.clone(), 1.4);
+        let mut mcts = Mcts::new(game.clone(), 1.4142356237);
         let action = mcts.search(1000);
         game = game.step(action);
         game.render();
